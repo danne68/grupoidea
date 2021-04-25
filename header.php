a0ead1d6c3ea61ce52b30b515339fc3d73977e9f
Daniel Solis <dsolis@xcaret.com>
2021-04-24 23:42:03 -0500

    primera fase menu mobil

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 	}
 ?>
 <header class="container mx-auto py-4 px-2 md:px-4 flex">
-  <div class="flex md:hidden items-center px-2 border mr-4"><i class="fa fa-bars fa-lg"></i></div>
+  <div id="buttonMob" class="flex md:hidden items-center px-2 border mr-4"><i class="fa fa-bars fa-lg"></i></div>
   <a href="index.php" class="w-1/4 md:w-auto items-center flex"><img class="w-full max-w-40" src="images/logo.png"></a>
   <ul class="headerNav hidden md:flex flex-auto justify-center items-center">
     <li class="inline-block mr-2 md:mr-4"><a class="uppercase flex" href="./">Inicio</a></li>
@@ -39,3 +39,48 @@
     <!--<li class="pl-2"><a href="#" class="fab fa-youtube fa-lg hover:text-red-600"></a></li>-->
   </ul>
 </header>
+<div id="menuMob">
+  <div id="menuWrapper" class="px-4 py-2">
+    <div id="menuMobClose" class="text-right"><i class="fa fa-times fa-2x"></i></div>
+    <ul class="pt-4">
+      <li class="py-2 uppercase"><a href="./" class="flex">Inicio</a></li>
+      <li class="py-2 uppercase"><a href="/somos.php" class="flex">¿Quiénes Somos?</a></li>
+      <li class="py-2 uppercase">
+        <div id="productMob" class="flex w-full">
+          <span class="flex-1">Productos</span>
+          <i class="flex items-center fa fa-chevron-down fa-lg"></i>
+        </div>
+        <?php if (sizeof($menuProd) != 0) { ?>
+          <ul id="productList" class="hidden pl-4 normal-case">
+            <?php foreach($menuProd as $menu) {?>
+              <li class="pl-2 pr-2 flex"><a class="py-2 w-full text-lg" href="/products.php?id=<?php echo $menu['id']; ?>"><?php echo $menu['categoria']; ?></a></li>
+            <?php } ?>
+          </ul>
+        <?php } ?>
+      </li>
+      <li class="py-2 uppercase"><a href="/contact.php" class="flex">Contacto</a></li>
+    </ul>
+  </div>
+</div>
+<script type='text/javascript' language='javascript'>
+  $('#buttonMob').click(function(){
+    $("body").addClass("noscroll");
+    document.getElementById("menuMob").style.width = "100%";
+    document.getElementById("menuWrapper").style.display = "block";
+	});
+
+  $('#menuMobClose').click(function(){
+    $("body").removeClass("noscroll");
+    document.getElementById("menuWrapper").style.display = "none";
+    document.getElementById("menuMob").style.width = "0";
+	});
+
+  var btn = document.querySelector('#productMob');
+  var btnIcon = document.querySelector("#productMob i");
+  btn.addEventListener('click', function(){
+    var list = document.getElementById('productList');
+    list.classList.toggle("hidden");
+    btnIcon.classList.toggle("fa-chevron-down");
+    btnIcon.classList.toggle("fa-chevron-up");
+  });
+</script>
